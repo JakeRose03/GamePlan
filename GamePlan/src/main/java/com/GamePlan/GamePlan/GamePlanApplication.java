f6d51d5f3058ad332d4cacf3a01ae550e5a3cbba
JakeRose03 <jake.rose8@icloud.com>
2025-09-16 21:02:43 -0400

    init commit spring setup

---
+++ GamePlan/src/main/java/com/GamePlan/GamePlan/GamePlanApplication.java
@@ -0,0 +1,13 @@
+package com.GamePlan.GamePlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePlanApplication.class, args);
+	}
+
+}
---
+++ GamePlan/src/test/java/com/GamePlan/GamePlan/GamePlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GamePlan.GamePlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
