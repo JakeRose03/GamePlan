3e2d15d7fa44861cb6ed09278600399c642a1931
JakeRose03 <jake.rose8@icloud.com>
2025-09-18 14:13:05 -0400

    Spring Data Jpa connected to postrges DB

--- GamePlan/src/main/java/com/GamePlan/GamePlan/GamePlanApplication.java
+++ GamePlan/src/main/java/com/GamePlan/GamePlan/GamePlanApplication.java
@@ -8,6 +8,7 @@ public class GamePlanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamePlanApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
